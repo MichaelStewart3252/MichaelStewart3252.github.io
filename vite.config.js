171e1bd5149fb040e5bb191709fa7f0c026ee2fe
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-03-16 14:10:52 -0400

    Fixed redundancy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    open: true // Optionally open the page automatically in your browser
+  }
 });
