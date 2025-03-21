a328d34d54eb51204bc0869fe765522fdbdb6e9e
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-03-21 14:44:51 -0400

    -dist/assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'; // Import the path module
 
 export default defineConfig({
-  base: '/MichaelStewart3252.github.io/',
+  base: '/MichaelStewart3252.github.io/', // Update this if your repo name is different
   plugins: [vue()],
   server: {
-    open: true
-  }
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // Explicitly set the entry point
+      },
+    },
+  },
 });
