915ee79fcd086ee5253dd3fbb93e3c4ed46fb8f6
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-03-22 18:54:48 -0400

    Gang shit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/MichaelStewart3252.github.io/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
