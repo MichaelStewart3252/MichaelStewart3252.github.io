5247667857e3b5aa71ff37990e5747ab518c6c25
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-03-22 18:56:58 -0400

    Gang shit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app" ref="appContainer">
     <header>
-      <h1>SKIBABIBIBI</h1>
       <nav>
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
