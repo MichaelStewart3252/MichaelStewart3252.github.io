373524531a734cde385acef2117a03a0054e2a0c
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-03-22 18:18:20 -0400

    repush

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/MichaelStewart3252.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
