9961c22f2a8a1851a3d8238a4071ad5407ab0af2
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-04-11 17:04:05 -0400

    resizing changes

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import postcssNormalize from 'postcss-normalize'
-import postcssFontMagician from 'postcss-font-magician'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          whitespace: 'preserve'
-        }
-      }
-    }),
+    vue(),
     vueDevTools(),
   ],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
-  css: {
-    devSourcemap: true,
-    postcss: {
-      plugins: [
-        postcssNormalize({
-          forceImport: true,
-          allowDuplicates: false
-        }),
-        postcssFontMagician({
-          display: 'swap',
-          foundries: 'google'
-        })
-      ]
-    }
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      format: {
-        comments: false
-      }
-    }
-  }
 })
