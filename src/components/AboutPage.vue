542c52ac7ad1cf60c6d3c62a8fa13adbcc0a6901
MichaelStewart3252 <123343771+MichaelStewart3252@users.noreply.github.com>
2025-04-27 20:49:34 -0400

    added new resume

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -9,7 +9,7 @@
       </p>
     </div>
 
-    <a href="/resume.pdf" target="_blank" class="airplane" aria-label="View Resume" ref="airplane">
+    <a href="/MichaelStewartResume.pdf" target="_blank" class="airplane" aria-label="View Resume" ref="airplane">
       <svg width="100" height="100" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
         <path d="M2 3L22 12L2 21L2 14L17 12L2 10L2 3Z" stroke="black" stroke-width="1"/>
       </svg>
